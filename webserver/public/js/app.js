40f872e1e540c352972d7c840130d9ca7e834eb7
Umashankar Pattamatta <upattamatta@planittesting.com>
2019-12-06 17:11:25 +1100

    final commit for the day

--- webserver/public/js/app.js
+++ webserver/public/js/app.js
@@ -1,1 +1,6 @@
 console.log('client side javascript loaded!!')
+fetch('http://localhost:3000/weather?address=boston').then(function(response){
+    response.json().then(function(data){
+        console.log(data);
+    })
+});
--- webserver/src/app.js
+++ webserver/src/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const location = require('./utils.js');
 
 //when you invoke that function it gives you an object
 const app = express();
@@ -25,17 +26,54 @@ hbs.registerPartials(partialsPath);
 console.log('The public directory is'+publicDir)
 app.use(express.static(publicDir));
 
+app.get('/weather',function(req,res){
+    if(!req.query.address)
+    {
+       return  res.send({'Error':'No Address Information provided'});
+    }
+    location.getGeoCode(req.query.address,function(error,gecodeResponse){
+        if(error){
+            return res.send({'Error:':error});
+        }
+        else
+        {
+            //call the get Forecast function 
+            location.getForecast(gecodeResponse.longitude,gecodeResponse.latitude,function(error,forecastResponse){
+                if(error){
+                    return res.error;
+                }
+                else{
+                        res.send({
+                            'Gecocode Details':gecodeResponse,
+                            'Forecaset Details':forecastResponse
+                        });
+                }
+            });
+        }
+    })
+});
+
 app.get('',function(req,res){
+    
     res.render('index',{
-        title:'Weather App',
+        title:'Weather',
         name: 'Umashankar'
     })
 });
 app.get('/about',function(req,res){
     res.render('about',{
-        title:'About Page',
+        title:'About me',
         name:'umashankar'
     })
+});
+app.get('/products',function(req,res){
+    //run only if there's no search term
+    if(!req.query.search){
+       return res.send({
+            error:'You must provide a search term'
+        })
+    }
+    res.send({products:['product1','product2','product3']});
 });
 app.get('/home',function(req,res){
     res.render('home',{
@@ -67,6 +105,19 @@ res.send('Hello Express!')
 app.get('/weather',function(req,res){
     res.send('You weather is perfect!');
 
+});
+app.get('/help/*',function(req,res)
+{
+    res.render('missinghelparticle',{
+        title:'Help page',
+        name:'umashankar'
+        });
+});
+app.get('*',function(req,res){
+    res.render('custom HTTP 404',{
+        title:'HTTP 404 page',
+        name:'Umashankar'
+    });
 });
 app.listen(3000,function(){
     console.log('server is up on port 3000!');
